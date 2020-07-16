5655e21ea76fae5e4e861465738e07e7d162d1b0
Justin Dongwook Jung <jungdw0624@gmail.com>
2020-07-16 22:46:03 +0900

    Initial Commit

---
+++ ImagePickerSample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ImagePickerSample
+//
+//  Created by justin dongwook Jung on 2020/07/16.
+//  Copyright © 2020 justin dongwook Jung. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
